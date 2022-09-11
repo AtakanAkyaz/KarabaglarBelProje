dce37e46253c4f9b4a731ec56ec1e5077add287d
AtakanAkyaz <61373558+AtakanAkyaz@users.noreply.github.com>
2022-09-11 23:05:27 +0300

    -Kullanıcı class ı oluşturuldu , paneller yapıldı ,

---
+++ KarabaglarBelediyesi/src/BilgisayarKontrolSistemi/kullanici.java
@@ -0,0 +1,30 @@
+package BilgisayarKontrolSistemi;
+
+public class kullanici {
+	String isim , sifre;
+	boolean flag;
+
+	public boolean isFlag() {
+		return flag;
+	}
+
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+
+	public String getIsim() {
+		return isim;
+	}
+
+	public void setIsim(String isim) {
+		this.isim = isim;
+	}
+
+	public String getSifre() {
+		return sifre;
+	}
+
+	public void setSifre(String sifre) {
+		this.sifre = sifre;
+	}
+}
--- KarabaglarBelediyesi/src/BilgisayarKontrolSistemi/system.java
+++ KarabaglarBelediyesi/src/BilgisayarKontrolSistemi/system.java
@@ -31,12 +31,15 @@ public class system {
 //		System.out.println(örnek.getWatt());
 //		örnek.setWatt(200);
 //		System.out.println(örnek.getWatt());
+		
+		kullanici kullanici1 = new kullanici();
+		kullanici1.setFlag(false);
+		kullanici kullanici2 = new kullanici();
+		kullanici2.setFlag(true);
+		
 		 
-		//***Güç Ünitesi Deneme***
-//		gucUnitesi pan = new gucUnitesi();
-//		System.out.println(pan.getWatt());
-//		pan.setWatt(200);
-//		System.out.println(pan.getWatt());
+		String adminDepoKullanıcıAdi = "kBel";
+		String adminDepoSifre = "@dm!N123";
 		
 		String depoKullanıcıAdi = "Atakan";
 		String depoSifre = "123";
@@ -47,11 +50,18 @@ public class system {
 			String kullanıcıAdi = scan.next();
 			System.out.println("Şifrenizi Giriniz : ");
 			String sifre = scan.next();
+//Normal kullanıcı girişi
 			if(depoKullanıcıAdi.equals(kullanıcıAdi) && depoSifre.equals(sifre)) {
 				System.out.println("Sisteme Hoş geldiniz");
-				KullaniciPaneli();
+				KullaniciPaneli(kullanici1);
 				break;
 			}
+//Admin girişi
+			else if(adminDepoKullanıcıAdi.equals(kullanıcıAdi) && adminDepoSifre.equals(sifre)) {
+				KullaniciPaneli(kullanici2);
+				
+			}
+//Yanlış isim veya şifre kullanımı
 			else {
 				System.out.println("Girdiğiniz kullanıcı adı veya şifre yanlış \n"
 						+ "Tekrar denemek için 1 e \n"
@@ -80,14 +90,43 @@ public class system {
 				
 	}
 	
-	public static void KullaniciPaneli() {
+	public static void AdminPaneli() {
+		System.err.println("Admin paneline hoş geldiniz");
+	}
+	
+	public static void KullaniciPaneli(kullanici kull) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Kullanıcı paneline hoş geldiniz ");
+		System.out.println("Kullanıcı paneline hoş geldiniz\n"
+				+ "Programları kontrol etmek için 1 e basınız\n"
+				+ "Kasa ile ilgili işlemler için 2 ye basınız");
 		int input = scan.nextInt();
 		switch(input) {
 		case 1:
+//programları kontrol etme
+			break;
+		case 2:
+//Kasa işlemleri
+			KasaIslemleri(kull);
+			break;
+		default:
+			System.out.println("Lütfen geçerli olan seçimlerden birini kullanınız");
+			break;
 		}
-				
+	}
+	
+	public static void ProgramKontrolu() {
+		
+	}
+	
+	public static void KasaIslemleri(kullanici kullanici) {
+		Scanner scan = new Scanner(System.in);
+		if(kullanici.isFlag()) {
+			System.out.println("kasa ekle veya kaldır");
+		}
+		System.out.println("program ekle kaldır\n"
+				+ "parça ekle kaldır");
+		int input = scan.nextInt();
+		
 	}
 
 }
